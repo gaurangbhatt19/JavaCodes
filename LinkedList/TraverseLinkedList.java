9bea8fa57d980fe3d62c948d428475dc26d19d56
GBSourceCodes <gaurangbhatt134@gmail.com>
2021-04-30 19:50:20 +0530

    Java

---
+++ LinkedList/TraverseLinkedList.java
@@ -0,0 +1,21 @@
+class Node{
+    int value;
+    Node next;
+    Node(int i){
+        value=i;
+        next=null;
+    }
+}
+public class TraverseLinkedList{
+public static void main(String[] args){
+    Node head= new Node(1);
+    head.next= new Node(2);
+    Node temp=head;
+    while(temp!=null)
+    {
+        System.out.println(temp.value);
+        temp=temp.next;
+    }
+    
+}
+}
